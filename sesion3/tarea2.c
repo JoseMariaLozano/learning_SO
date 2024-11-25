85f191311c6047158aa3ec9146fbc7553c9d6904
Jose Maria <joseloum3@gmail.com>
2024-11-25 13:45:16 +0100

    Modified tarea2.c

--- sesion3/tarea2.c
+++ sesion3/tarea2.c
@@ -1,35 +1,60 @@
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<stdio.h>
-#include<unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 int main()
 {
-  int nproc = 5;
-  int childpid;
-  for (size_t i = 0; i < nproc; ++i)
-  {
-    if( ( childpid = fork() ) < 0)
+    int nproc = 5;
+    int childpid;
+    pid_t hijos[nproc]; 
+    for (size_t i = 0; i < nproc; ++i)
     {
-      perror("Error en el fork (creación de procesos hijo)");
-      exit(EXIT_FAILURE);
+        if ((childpid = fork()) < 0)
+        {
+            perror("Error en el fork (creación de procesos hijo)");
+            exit(EXIT_FAILURE);
+        }
+
+        if (!childpid)
+        {
+            printf("Soy el hijo con pid %d\n", getpid());
+            exit(EXIT_SUCCESS); 
+        }
+        else
+        {
+            hijos[i] = childpid; 
+        }
     }
 
-    if(!childpid)
+    if (childpid)
     {
-      printf( "Soy el hijo con pid %d\n", getpid() );
-      break;
-    }
-  }
+        pid_t pid;
+        int hijos_vivos = nproc;
 
-  if(childpid){
-    pid_t pid;
-    while( (pid = wait(NULL) ) > 0)
-    {
-      printf( "Acaba de finalizar mi hijo con pid %d\n", pid );
-      printf( "Solo me quedan %d hijos vivos\n", --nproc)
-    }
-  }
-  
+        
+        for (size_t i = 0; i < nproc; i += 2)
+        {
+            pid = waitpid(hijos[i], NULL, 0); 
+            if (pid > 0)
+            {
+                printf("Acaba de finalizar mi hijo impar con pid %d\n", pid);
+                printf("Solo me quedan %d hijos vivos\n", --hijos_vivos);
+            }
+        }
 
-  return 0;
+        
+        for (size_t i = 1; i < nproc; i += 2)
+        {
+            pid = waitpid(hijos[i], NULL, 0); 
+            if (pid > 0)
+            {
+                printf("Acaba de finalizar mi hijo par con pid %d\n", pid);
+                printf("Solo me quedan %d hijos vivos\n", --hijos_vivos);
+            }
+        }
+    }
+
+    return 0;
 }
